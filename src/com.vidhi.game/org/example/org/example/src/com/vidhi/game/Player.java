a10a0f5212f91f55f21a035726cb21b2b3e160b7
vidhi-sareen <87638698+vidhi-sareen@users.noreply.github.com>
2023-04-12 18:57:53 +0530

    Update Player.java

--- src/com.vidhi.game/org/example/org/example/src/com/vidhi/game/Player.java
+++ src/com.vidhi.game/org/example/org/example/src/com/vidhi/game/Player.java
@@ -1,24 +1,24 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Player {
 
     private int id;
     // cards which players will be having in their hand.
-    private ArrayList<Card> cards = new ArrayList<>();
+    private Set<Card> cards = new HashSet<>();
 
     // Player constructor with passing as his id in parameter.
     public Player(int id) {
         this.id = id;
     }
+    
     /*
      * this is for adding card to the player's
      * hand after if the card is not matched with the top of
      * discard pile or after any action card (Q,J) played by the
      * previous player.
      */
-
     public void addCard(Card card) {
-
         cards.add(card);
     }
 
@@ -27,24 +27,25 @@ public class Player {
      * when any of the player's card matches with the
      * top of the discard pile.
      */
-
-    public void removeCard(Card card) {
-        cards.remove(card);
-        return;
+    public boolean removeCard(Card card) {
+        return cards.remove(card);
     }
 
     // to get the id of the player
-    public int giveId() {
+    public int getId() {
         return id;
     }
 
     // to get the player's hands cards.
-    public ArrayList<Card> giveCards() {
+    public Set<Card> getCards() {
         return cards;
     }
 
+    @Override
     public String toString() {
-        return id + " " + cards.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append(id).append(" ");
+        sb.append(cards.toString());
+        return sb.toString();
     }
-
 }
